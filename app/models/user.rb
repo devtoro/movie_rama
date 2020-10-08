90b2912878cba73cdf9161f5b12824b7f01aaff5
Manolis <mtsilikidis@dialecticanet.com>
2020-10-08 15:14:17 +0300

    Pass users validations specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,22 @@
 class User < ApplicationRecord
+  # for bcrypt and authentication
+  has_secure_password
+  # Relationships
+  has_many :movies
+  has_many :movie_reactions
+  has_many :reactions, through: :movie_reactions
+
+  # Validations
+  VALID_EMAIL_REGEX = /\A\w+([\'.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,6})+\z/.freeze
+  validates :full_name, presence: true
+  validates :email,
+            presence: true,
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 8 }
+
+  # Callbakcs
+  before_save { self.email = email.downcase }
 end
 
 # == Schema Information
