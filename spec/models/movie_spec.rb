e6f7437dc745c495dfbcda029fa0de16ca524b4b
Manolis <mtsilikidis@dialecticanet.com>
2020-10-08 17:49:41 +0300

    Update validation specs

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe Movie, type: :model do
   context 'Validations' do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:user).on(:create) }
+    it { should validate_presence_of(:user_id) }
     it { should have_db_column(:user_id) }
-    it { should belong_to(:user) }
   end
 end
