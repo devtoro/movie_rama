7665220758e9fc16edb95c841fa10f42dc622aa3
Manolis <mtsilikidis@dialecticanet.com>
2020-10-09 16:32:59 +0300

    Improve movie view specs

--- spec/views/movies/_movie.html.haml_spec.rb
+++ spec/views/movies/_movie.html.haml_spec.rb
@@ -1,15 +1,18 @@
 RSpec.describe 'movies/_movies.html.haml' do
   context 'Reactions' do
-    before(:each) do
-      ['like'].each { |r| Reaction.create(name: r) }
-    end
+    let(:reactions) { ['like'].map{ |r| Reaction.create(name: r) } }
 
     it 'Has reaction buttons disabled if user shared the respective movie' do
       movie = FactoryBot.create(:movie)
 
       view.stub(:current_user) { movie.user }
 
-      render partial: 'movies/movie.html.haml', locals: { movie: movie }
+      render  partial:
+              'movies/movie.html.haml',
+              locals: {
+                movie: movie,
+                reactions: reactions
+              }
 
       expect(rendered).not_to have_link 'Like', href: movie_reactions_path
       expect(rendered).to have_text 'Like'
@@ -20,7 +23,12 @@ RSpec.describe 'movies/_movies.html.haml' do
 
       view.stub(:current_user) { FactoryBot.create(:user) }
 
-      render partial: 'movies/movie.html.haml', locals: { movie: movie }
+      render  partial:
+              'movies/movie.html.haml',
+              locals: {
+                movie: movie,
+                reactions: reactions
+              }
 
       expect(rendered).to have_link 'Like', href: movie_reactions_path
     end
