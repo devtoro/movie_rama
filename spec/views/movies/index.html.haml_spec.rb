7fbfeaf73327f9a25aab0f65c40e331fe73c7654
Manolis <mtsilikidis@dialecticanet.com>
2020-10-09 17:28:20 +0300

    Update spec

--- spec/views/movies/index.html.haml_spec.rb
+++ spec/views/movies/index.html.haml_spec.rb
@@ -1,6 +1,20 @@
 RSpec.describe 'movies/index.html.haml' do
   context 'Top filters' do
-    it 'Has sorting option for date'
-    it 'Has sorting options for all reactions that exist in the database'
+    before(:each) do
+      assign(:reactions, %w[like hate].map { |r| Reaction.create(name: r) })
+      assign(:movies, [FactoryBot.create(:movie)])
+    end
+
+    it 'Has sorting option for date' do
+      render
+
+      expect(rendered).to have_link 'Date', href: '/movies?dir=desc&order=date'
+    end
+    it 'Has sorting options for all reactions that exist in the database' do
+      render
+
+      expect(rendered).to have_link 'Likes', href: '/movies?dir=desc&order=like'
+      expect(rendered).to have_link 'Hates', href: '/movies?dir=desc&order=hate'
+    end
   end
 end
