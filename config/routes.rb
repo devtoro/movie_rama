d4c055d40c674b7d50571e1c1652872f26be63ac
Manolis <mtsilikidis@dialecticanet.com>
2020-10-09 11:39:23 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :movies
+  resources :users
+  resources :sessions, only: %i[create destroy]
+  resources :movie_reactions, only: %i[create update destroy]
+
+  root to: 'movies#index'
 end
