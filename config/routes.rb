c57e687cbda3267e1c28103599ccc03c84b1b150
Manolis <mtsilikidis@dialecticanet.com>
2020-10-09 15:22:57 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :movies
-  resources :users
-  resources :sessions, only: %i[create destroy]
+  resources :users, except: :new
+  resources :sessions, only: %i[new create destroy]
   resources :movie_reactions, only: %i[create update destroy]
+  get '/signup', to: 'users#new'
 
   root to: 'movies#index'
 end
