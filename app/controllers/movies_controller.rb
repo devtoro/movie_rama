3558d7bcf85b2d815ccc53c877983ae2e79039e5
Manolis <mtsilikidis@dialecticanet.com>
2020-10-09 15:24:16 +0300

    Inherit from secure controller and fetch some data

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,6 @@
-class MoviesController < ApplicationController
+class MoviesController < SecureController
+  def index
+    @reactions = Reaction.all
+    @movies = Movie.includes(:user).all
+  end
 end
