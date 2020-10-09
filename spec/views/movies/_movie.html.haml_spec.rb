5f13e6e0b2fe1e7109c5c024c011d54e6fb00cfa
Manolis <mtsilikidis@dialecticanet.com>
2020-10-09 16:17:52 +0300

    Write specs for reaction buttons rendering

--- spec/views/movies/_movie.html.haml_spec.rb
+++ spec/views/movies/_movie.html.haml_spec.rb
@@ -1,5 +1,28 @@
 RSpec.describe 'movies/_movies.html.haml' do
   context 'Reactions' do
-    it 'Has reaction buttons disabled if user shared the respective movie'
+    before(:each) do
+      ['like'].each { |r| Reaction.create(name: r) }
+    end
+
+    it 'Has reaction buttons disabled if user shared the respective movie' do
+      movie = FactoryBot.create(:movie)
+
+      view.stub(:current_user) { movie.user }
+
+      render partial: 'movies/movie.html.haml', locals: { movie: movie }
+
+      expect(rendered).not_to have_link 'Like', href: movie_reactions_path
+      expect(rendered).to have_text 'Like'
+    end
+
+    it 'Has reactions buttons for any movie, except for ones shared by user' do
+      movie = FactoryBot.create(:movie)
+
+      view.stub(:current_user) { FactoryBot.create(:user) }
+
+      render partial: 'movies/movie.html.haml', locals: { movie: movie }
+
+      expect(rendered).to have_link 'Like', href: movie_reactions_path
+    end
   end
 end
