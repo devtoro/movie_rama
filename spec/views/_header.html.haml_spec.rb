97c47fa2f81fd78de81ce70220a26b190da5c92f
Manolis <mtsilikidis@dialecticanet.com>
2020-10-09 15:39:10 +0300

    Add header spec cases

--- spec/views/_header.html.haml_spec.rb
+++ spec/views/_header.html.haml_spec.rb
@@ -1,7 +1,37 @@
 RSpec.describe 'layouts/_header.html.haml' do
   context 'Profile section' do
-    it 'Shows login/signup buttons if user not authenticated'
-    it 'Shows welcome message if user is authenticated'
-    it 'Shows "New Movie" button if user is authenticated'
+    it 'Shows signup link if user not authenticated' do
+      view.stub(:current_user) { nil }
+
+      render
+
+      expect(rendered).to have_link 'Sign Up', href: '/signup'
+    end
+
+    it 'Shows login link if user not authenticated' do
+      view.stub(:current_user) { nil }
+
+      render
+
+      expect(rendered).to have_link 'Login', href: '/sessions/new'
+    end
+
+    it 'Shows new movie button if user is authenticated' do
+      current_user = FactoryBot.build(:user)
+      view.stub(:current_user) { current_user }
+
+      render
+
+      expect(rendered).to have_link 'New Movie', href: '/movies/new'
+    end
+
+    it 'Shows new welcome message if user is authenticated' do
+      current_user = FactoryBot.build(:user)
+      view.stub(:current_user) { current_user }
+
+      render
+
+      rendered.should contain "Welcome #{current_user.full_name}"
+    end
   end
 end
