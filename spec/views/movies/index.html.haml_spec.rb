b3ff8ca5ba107af1e6b2201a01beedf0d66680f0
Manolis <mtsilikidis@dialecticanet.com>
2020-10-09 18:30:36 +0300

    Enrich test for ordering direction

--- spec/views/movies/index.html.haml_spec.rb
+++ spec/views/movies/index.html.haml_spec.rb
@@ -13,8 +13,13 @@ RSpec.describe 'movies/index.html.haml' do
     it 'Has sorting options for all reactions that exist in the database' do
       render
 
-      expect(rendered).to have_link 'Likes', href: '/movies?dir=desc&order=like'
-      expect(rendered).to have_link 'Hates', href: '/movies?dir=desc&order=hate'
+      link1 = have_link('Likes', href: '/movies?dir=desc&order=like')
+      link2 = have_link('Likes', href: '/movies?dir=asc&order=like')
+      link3 = have_link('Hates', href: '/movies?dir=desc&order=hate')
+      link4 = have_link('Hates', href: '/movies?dir=asc&order=hate')
+
+      expect(rendered).to link1 || link2
+      expect(rendered).to link3 || link4
     end
   end
 end
