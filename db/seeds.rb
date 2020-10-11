dae2343a5130f11eb957491ef6eb0171db0cc76e
Manolis <mtsilikidis@dialecticanet.com>
2020-10-12 00:30:52 +0300

    Add seeds for User, Movie, Reaction

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create Users
+#
+[
+  {
+    full_name: 'Luke Skywalker',
+    email: 'luke@jedi.com',
+    password: 'movierama'
+  },
+  {
+    full_name: 'Frodo Baggins',
+    email: 'frodo@lotr.com',
+    password: 'movierama'
+  }
+].each { |user_attrs| User.create user_attrs }
+
+# Create movies
+#
+[
+  {
+    title: 'Ironman',
+    description: 'This is ironamn. It is a gem from MARVEL',
+    user: User.first
+  },
+  {
+    title: 'Gladiator',
+    description: 'This is Gladiator. An epic adventure of a man in roman times',
+    user: User.first
+  },
+  {
+    title: 'The Rockstar',
+    description: 'This is the rockstar. About a kid who followed his dream',
+    user: User.last
+  },
+  {
+    title: 'The Pick of Destiny',
+    description: 'This is the pick of destiny. About another kid whoh followe his dream, but a totally different concept',
+    user: User.last
+  }
+].each { |movie_attrs| Movie.create movie_attrs }
+
+# Create reactions
+#
+[
+  { name: 'like' },
+  { name: 'hate' }
+].each { |reaction_attrs| Reaction.create reaction_attrs }
