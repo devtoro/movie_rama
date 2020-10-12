da98b05a52a74a9b84c244f4429e0a063dd5ed0b
Manolis <mtsilikidis@dialecticanet.com>
2020-10-12 22:55:39 +0300

    Add functionality for movie reactions

--- app/controllers/movie_reactions_controller.rb
+++ app/controllers/movie_reactions_controller.rb
@@ -1,2 +1,52 @@
 class MovieReactionsController < SecureController
+  before_action :set_movie_reaction, only: %i[update destroy]
+
+  def create
+    @mr = MovieReaction.new(movie_reaction_params)
+
+    if @mr.save
+      respond_to do |format|
+        format.js
+      end
+    else
+      flash[:error] = @mr.errors.messages
+      render template: 'movies/index', status: 422
+    end
+  end
+
+  def update
+    if @mr.update_attributes(movie_reaction_params)
+      respond_to do |format|
+        format.js
+      end
+    else
+      flash[:error] = @mr.errors.messages
+      render template: 'movies/index', status: 422
+    end
+  end
+
+  def destroy
+    if @mr.destroy
+      respond_to do |format|
+        format.js
+      end
+    else
+      flash[:error] = @mr.errors.messages
+      render template: 'movies/index', status: 422
+    end
+  end
+
+  private
+
+  def movie_reaction_params
+    params.require(:movie_reaction).permit(
+      :user_id,
+      :movie_id,
+      :reaction_id
+    )
+  end
+
+  def set_movie_reaction
+    @mr = MovieReaction.find(params[:id])
+  end
 end
