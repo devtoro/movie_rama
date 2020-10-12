df772c03bc8f6983d6d2bcda3accfaff24e3a51d
Manolis <mtsilikidis@dialecticanet.com>
2020-10-12 12:02:37 +0300

    Create basic custom exception for unauthorized actions

---
+++ lib/unauthorized_exception.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Custom exception for unauthorized actions
+class UnauthorizedException < StandardError
+  def message
+    'You are not allowed to perform this action'
+  end
+end
