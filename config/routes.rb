69317607e38bd1409c3833396c0991795522464d
Manolis <mtsilikidis@dialecticanet.com>
2020-10-12 09:34:34 +0300

    Add login/logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   resources :movies
   resources :users, except: :new
-  resources :sessions, only: %i[new create destroy]
+  resources :sessions, only: %i[create]
   resources :movie_reactions, only: %i[create update destroy]
-  get '/signup', to: 'users#new'
+
+  get '/signup', to: 'users#create'
+
+  get '/login', to: 'sessions#new'
+  delete '/logout', to: 'sessions#destroy'
 
   root to: 'movies#index'
 end
