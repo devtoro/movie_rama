632bceb01601454fcf4e1d6d1bc18d373eef48a0
Manolis <mtsilikidis@dialecticanet.com>
2020-10-12 09:36:54 +0300

    Add actions for login/logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,23 @@
 class SessionsController < ApplicationController
-  def new
+  def create
+    user = User.find_by(email: login_params[:email].try(:downcase))
+    if user && user.authenticate(login_params[:password])
+      log_in user
+      redirect_back_or root_path
+    else
+      flash[:error] = 'Invalid email/password combination'
+      render template: 'sessions/new'
+    end
+  end
 
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+
+  private
+
+  def login_params
+    params.require(:session).permit(:email, :password)
   end
 end
