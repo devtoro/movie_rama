254755ac3c3244bd5ea945a120a2bdfa1a6b7ef0
Manolis <mtsilikidis@dialecticanet.com>
2020-10-12 12:03:51 +0300

    Rescue from UnauthorizedException

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+
+  rescue_from UnauthorizedException, with: :user_unauthorized
+
+
+  private
+
+  def user_unauthorized
+    flash[:error] = 'You are not authorized to perform this action.'
+    redirect_to(request.referrer || root_path)
+  end
 end
