bc15273fe716785b93ee7ba3da835c9f5c31f67c
Manolis <mtsilikidis@dialecticanet.com>
2020-10-12 09:52:06 +0300

    Change flash msg and improve code quality

--- app/controllers/secure_controller.rb
+++ app/controllers/secure_controller.rb
@@ -1,13 +1,13 @@
 class SecureController < ApplicationController
-  before_action :authenticated_user, only: [:create, :update, :destroy]
+  before_action :authenticated_user, only: %i[create update destroy]
 
   private
 
   def authenticated_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in"
-      redirect_to new_session_path
-    end
+    return if logged_in?
+
+    store_location
+    flash[:error] = 'Please log in first'
+    redirect_to new_session_path
   end
 end
