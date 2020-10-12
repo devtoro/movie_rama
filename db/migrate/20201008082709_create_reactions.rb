3d43a9613c364be5ce071ebd9c3ed930ed7d1347
Manolis <mtsilikidis@dialecticanet.com>
2020-10-12 22:58:53 +0300

    Alter column name

--- db/migrate/20201008082709_create_reactions.rb
+++ db/migrate/20201008082709_create_reactions.rb
@@ -2,7 +2,7 @@ class CreateReactions < ActiveRecord::Migration[6.0]
   def change
     create_table :reactions do |t|
       t.string :name, null: false
-      t.string :icon
+      t.string :color
 
       t.timestamps
     end
