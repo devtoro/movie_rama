57a78672c087a15922836e9c7222466d359770d3
Manolis <mtsilikidis@dialecticanet.com>
2020-10-15 00:32:44 +0300

    Add forgotten cache clearing

--- movie_rama/app/models/reaction.rb
+++ movie_rama/app/models/reaction.rb
@@ -29,8 +29,9 @@ class Reaction < ApplicationRecord
   # would end up with a similar setup, this callback should execute and
   # asynchronous job that would clear the required cache keys.
   def clear_cache_movies_counts
-    Rails.cache.delete_matched(/\d+_reaction_counts/)
+    Rails.cache.delete_matched(/\d*_reaction_counts/)
     Rails.cache.delete('all_reactions')
+    Rails.cache.delete('reactions_mapping')
   end
 end
 
